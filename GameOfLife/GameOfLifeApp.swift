6eb4543b8bac10644ebf649dba9ece8040c068f3
superkookai <weerawut@live.com>
2025-05-07 11:29:46 +0700

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Weerawut Chaiyasomboon on 07/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
